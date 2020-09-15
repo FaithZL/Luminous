d9061bbf115fe145cfbd12db1c743ecdd2c94b50
Zero <249002031@qq.com>
2020-09-15 22:42:07 +0800

    完善logging模块

--- Luminous/src/main.cpp
+++ Luminous/src/main.cpp
@@ -4,7 +4,7 @@
 
 #include "math/math_util.h"
 #include "compute/type_desc.h"
-
+#include "util/logging.h"
 
 using namespace std;
 
@@ -12,9 +12,6 @@ using namespace luminous;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-
-
-
 
     return 0;
 }
--- Luminous/src/util/string_print.h
+++ Luminous/src/util/string_print.h
@@ -4,125 +4,77 @@
 
 #pragma once
 
-inline void stringPrintfRecursive(std::string *s, const char *fmt) {
-    const char *c = fmt;
-    // No args left; make sure there aren't any extra formatting
-    // specifiers.
-    while (*c) {
-        if (*c == '%') {
-//            CHECK_EQ(c[1], '%');
-            ++c;
+
+#include <string>
+#include <string_view>
+#include <sstream>
+#include <fstream>
+#include <array>
+#include <filesystem>
+
+namespace luminous {
+inline namespace utility {
+
+template<typename ...Args>
+inline std::string serialize(Args &&...args) noexcept {
+    std::ostringstream ss;
+    static_cast<void>((ss << ... << std::forward<Args>(args)));
+    return ss.str();
+}
+
+inline std::string pascal_to_snake_case(std::string_view s) noexcept {  // TODO: Robustness
+    std::string result;
+    auto lower_met = false;
+    for (auto i = 0u; i < s.size(); i++) {
+        auto c = s[i];
+        if (std::isupper(c)) {
+            if (lower_met || (i != 0u && i != s.size() - 1u && !std::isupper(s[i + 1]))) { result.push_back('_'); }
+            lower_met = false;
+        } else {
+            lower_met = true;
         }
-        *s += *c++;
+        result.push_back(std::tolower(c));
     }
+    return result;
 }
 
-// 1. Copy from fmt to *s, up to the next formatting directive.
-// 2. Advance fmt past the next formatting directive and return the
-//    formatting directive as a string.
-inline std::string copyToFormatString(const char **fmt_ptr, std::string *s) {
-    const char *&fmt = *fmt_ptr;
-    while (*fmt) {
-        if (*fmt != '%') {
-            *s += *fmt;
-            ++fmt;
-        } else if (fmt[1] == '%') {
-            // "%%"; let it pass through
-            *s += '%';
-            *s += '%';
-            fmt += 2;
-        } else
-            // fmt is at the start of a formatting directive.
-            break;
+inline std::string text_file_contents(const std::filesystem::path &file_path) {
+    std::ifstream file{file_path};
+    if (!file.is_open()) {
+        std::ostringstream ss;
+        ss << "Failed to open file: " << file_path;
+        throw std::runtime_error{ss.str()};
     }
-
-    std::string nextFmt;
-    if (*fmt) {
-        do {
-            nextFmt += *fmt;
-            ++fmt;
-            // Incomplete (but good enough?) test for the end of the
-            // formatting directive: a new formatting directive starts, we
-            // hit whitespace, or we hit a comma.
-        } while (*fmt && *fmt != '%' && !isspace(*fmt) && *fmt != ',' &&
-                 *fmt != '[' && *fmt != ']' && *fmt != '(' && *fmt != ')');
-    }
-
-    return nextFmt;
+    return {std::istreambuf_iterator<char>{file}, std::istreambuf_iterator<char>{}};
 }
 
-template <typename T>
-inline std::string formatOne(const char *fmt, T v) {
-    // Figure out how much space we need to allocate; add an extra
-    // character for the '\0'.
-    size_t size = snprintf(nullptr, 0, fmt, v) + 1;
-    std::string str;
-    str.resize(size);
-    snprintf(&str[0], size, fmt, v);
-    str.pop_back();  // remove trailing NUL
-    return str;
+}
 }
 
-// General-purpose version of stringPrintfRecursive; add the formatted
-// output for a single StringPrintf() argument to the final result string
-// in *s.
-template <typename T, typename... Args>
-inline void stringPrintfRecursive(std::string *s, const char *fmt, T v,
-                                  Args... args) {
-    std::string nextFmt = copyToFormatString(&fmt, s);
-    *s += formatOne(nextFmt.c_str(), v);
-    stringPrintfRecursive(s, fmt, args...);
+#ifdef __OBJC__
+
+#import <Foundation/Foundation.h>
+
+namespace luisa { inline namespace utility {
+
+[[nodiscard]] inline NSString *make_objc_string(const char *s) noexcept {
+    return [[NSString alloc] initWithCString:s encoding:NSUTF8StringEncoding];
 }
 
-// Special case of StringPrintRecursive for float-valued arguments.
-template <typename... Args>
-inline void stringPrintfRecursive(std::string *s, const char *fmt, float v,
-                                  Args... args) {
-    std::string nextFmt = copyToFormatString(&fmt, s);
-    if (nextFmt == "%f")
-        // Always use enough precision so that the printed value gives
-        // the exact floating-point value if it's used to initialize a
-        // float.
-        // https://randomascii.wordpress.com/2012/03/08/float-precisionfrom-zero-to-100-digits-2/
-        *s += formatOne("%.9g", v);
-    else
-        // If a specific formatting string other than "%f" was specified,
-        // just use that.
-        *s += formatOne(nextFmt.c_str(), v);
-
-    // Go forth and print the next arg.
-    stringPrintfRecursive(s, fmt, args...);
+[[nodiscard]] inline NSString *make_objc_string(std::string_view sv) noexcept {
+    return [[NSString alloc] initWithBytes:sv.data() length:sv.size() encoding:NSUTF8StringEncoding];
 }
 
-// Specialization for doubles that always uses enough precision.  (It seems
-// that this is the version that is actually called for floats.  I thought
-// that float->double promotion wasn't supposed to happen in this case?)
-template <typename... Args>
-inline void stringPrintfRecursive(std::string *s, const char *fmt, double v,
-                                  Args... args) {
-    std::string nextFmt = copyToFormatString(&fmt, s);
-    if (nextFmt == "%f")
-        *s += formatOne("%.17g", v);
-    else
-        *s += formatOne(nextFmt.c_str(), v);
-    stringPrintfRecursive(s, fmt, args...);
+[[nodiscard]] inline std::string to_string(NSString *s) noexcept {
+    return s.UTF8String;
 }
 
-// StringPrintf() is a replacement for sprintf() (and the like) that
-// returns the result as a std::string. This gives convenience/control
-// of printf-style formatting in a more C++-ish way.
-//
-// Floating-point values with the formatting string "%f" are handled
-// specially so that enough digits are always printed so that the original
-// float/double can be reconstituted exactly from the printed digits.
-template <typename... Args>
-inline std::string StringPrintf(const char *fmt, Args... args) {
-    std::string ret;
-    stringPrintfRecursive(&ret, fmt, args...);
-    return ret;
+[[nodiscard]] inline std::string_view to_string_view(NSString *s) noexcept {
+    return {s.UTF8String, s.length};
 }
 
-#ifdef __GNUG__
-#pragma GCC diagnostic pop
-#endif  // __GNUG__
+}}
+
+#endif
+
 
