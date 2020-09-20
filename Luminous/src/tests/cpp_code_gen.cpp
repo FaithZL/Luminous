b6873ea619d19aff59550cbe09266c37be4411e0
Zero <249002031@qq.com>
2020-09-20 23:22:11 +0800

    整理工程依赖关系

---
+++ Luminous/src/tests/cpp_code_gen.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Zero on 2020/9/20.
+//
+
+#include <iostream>
+#include "ext/nlohmann/json.hpp"
+#include "core/math/data_types.h"
+
+#include "compute/type_desc.h"
+#include "core/logging.h"
+//#include "compute/buffer.h"
+#include "compute/dsl.h"
+
+using namespace std;
+
+using namespace luminous;
+
+int main() {
+    LUMINOUS_INFO(123, "dasf");
+
+    return 0;
+}
