97fd44f2bb1e8f97960026d197cb5ace616c1f0c
Zero <249002031@qq.com>
2020-09-21 23:41:04 +0800

    Update cpp_code_gen.cpp

--- Luminous/src/tests/cpp_code_gen.cpp
+++ Luminous/src/tests/cpp_code_gen.cpp
@@ -2,21 +2,18 @@
 // Created by Zero on 2020/9/20.
 //
 
-#include <iostream>
-#include "ext/nlohmann/json.hpp"
-#include "core/math/data_types.h"
-
-#include "compute/type_desc.h"
-#include "core/logging.h"
-//#include "compute/buffer.h"
-#include "compute/dsl.h"
+#include <compute/dsl.h>
+#include <compute/codegen.h>
+#include <compute/buffer.h>
 
 using namespace std;
 
 using namespace luminous;
 
 int main() {
-    LUMINOUS_INFO(123, "dasf");
+    variant<int , float> a = 1;
+
+    cout << a.valueless_by_exception() << endl;
 
     return 0;
 }
