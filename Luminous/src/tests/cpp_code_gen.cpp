591bb68121f2f70e19d0561b337d17010e9ea97a
Zero <249002031@qq.com>
2020-09-25 23:48:17 +0800

    添加context

--- Luminous/src/tests/cpp_code_gen.cpp
+++ Luminous/src/tests/cpp_code_gen.cpp
@@ -8,6 +8,7 @@
 #include "compute/device.h"
 #include "compute/function.h"
 #include "compute/codegen.h"
+#include "core/math/data_types.h"
 
 using namespace std;
 
@@ -16,6 +17,43 @@ using namespace luminous;
 using namespace luminous::compute;
 
 using namespace luminous::compute::dsl;
+
+void test() {
+    int v1{0};
+    float v2{static_cast<float>(0.0f / +0.0f)};
+    if (((v1 > 0) && (v2 < 0))) {
+        v2 = 0;
+    } else if ((v2 > 0)) {
+        v1 = 0;
+    } else {
+        v1 = 0;
+    }
+    while (v1) {
+        v1 += 1;
+    }
+    float2 v3{luminous::make_float2(0)};
+    float v4{sqrt((v3.x))};
+    float v5{(0x1.921fb6p+2f * (v3.y))};
+    float3 v6{make_float3((v4 * cos(v5)), (v4 * sin(v5)), sqrt((0x1p+0f - (v3.x))))};
+    switch (v1) {
+        case 1: {
+            v1 = 0;
+            break;
+        }
+        case 2: {
+            v2 = 0;
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+    int v7{5};
+    do {
+        v7 = 5;
+        break;
+    } while ((v1 < 10));
+}
 
 template<typename Def, std::enable_if_t<std::is_invocable_v<Def>, int> = 0>
 void fake_compile_kernel(std::string name, Def &&def) {
@@ -29,23 +67,54 @@ void fake_compile_kernel(std::string name, Def &&def) {
     LUMINOUS_INFO("Done.");
 }
 
-void test() {
-    int v1{0};
-    float v2{static_cast<float>(0.0f / +0.0f)};
-    if (((v1 > 0) && (v2 < 0))) {
-        v2 = 0;
-    }
+inline Expr<float3> cosine_sample_hemisphere(Expr<float2> u) {
+    Var r = sqrt(u.x());
+    Var phi = static_cast<float>(2.0f * M_PI) * u.y();
+    return make_float3(r * cos(phi), r * sin(phi), sqrt(1.0f - u.x()));
 }
 
 int main() {
 
     fake_compile_kernel("test", [](){
+        auto tid = thread_id();
+
         Var<int> a = 0;
         Var<float> b = 0.f / 0.f;
         If(a > 0 && b < 0) {
             b = 0;
+        } Elif (b > 0) {
+            a = 0;
+        } Else {
+            a = 0;
         };
+
+        While(a) {
+            a += 1;
+        };
+
+        Var<float2> c = make_float2(0);
+
+        Var aa = cosine_sample_hemisphere(c);
+
+        Switch(a) {
+            Case(1) {
+                a = 0;
+            };
+            Case(2) {
+                b = 0;
+            };
+            Default {
+
+            };
+        };
+        Var i = 5;
+        Do {
+            i = 5;
+            Break;
+        } When(a < 10);
     });
+
+
 
     float v2{0x1.99999ap-4f};
     cout << v2;
