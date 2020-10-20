f4421ce0117fa6a652d1aa7c56a9f283ec4cf646
Zero <249002031@qq.com>
2020-10-20 23:54:43 +0800

    添加filter

--- Luminous/src/render/include/filter.h
+++ Luminous/src/render/include/filter.h
@@ -3,3 +3,32 @@
 //
 
 #pragma once
+
+#include <render/plugin.h>
+#include <compute/dsl.h>
+
+namespace luminous::render {
+
+    using compute::Device;
+
+    class Filter : public Plugin {
+    private:
+        float _radius;
+    public:
+        Filter(Device *device, const nloJson &params)
+        : Plugin(device, params),
+        _radius(params.value("radius", 1)) {
+
+        }
+
+        [[nodiscard]] float radius() const noexcept {
+            return _radius;
+        }
+
+        // Filter 1D weight function, offset is in range [-radius, radius)
+        [[nodiscard]] virtual float _weight_1d(float offset) const noexcept = 0;
+
+        // (position, weight)
+        [[nodiscard]] virtual std::pair<Expr<float2>, Expr<float>> importance_sample_pixel_position(Expr<uint2> p, Expr<float2> u) = 0;
+    };
+}
