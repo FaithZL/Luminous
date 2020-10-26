8a6ad02988632eac38072e98ddcb0023eeb36950
Zero <249002031@qq.com>
2020-10-26 23:21:40 +0800

    add pipeline

---
+++ Luminous/src/compute/pipeline.h
@@ -0,0 +1,59 @@
+//
+// Created by Zero on 2020/10/26.
+//
+
+#pragma once
+
+#include <compute/device.h>
+#include <compute/kernel.h>
+#include <compute/dispatcher.h>
+
+namespace luminous::compute {
+
+    class Pipeline : public Noncopyable {
+    public:
+        static constexpr auto max_stages_in_queue = 32u;
+    private:
+        Device *_device;
+        std::queue<std::function<void(Dispatcher &)>> _stages;
+    public:
+        explicit Pipeline(Device *device) : _device{device} {}
+
+        [[nodiscard]] Device *device() const noexcept { return _device; }
+
+        template<typename Func,
+                std::enable_if_t<std::is_invocable_v<Func, Dispatcher &>, int> = 0>
+        Pipeline &operator<<(Func &&func) {
+            _stages.emplace(std::forward<Func>(func));
+            if (_stages.size() >= max_stages_in_queue) {
+                run();
+            }
+            return *this;
+        }
+
+        template<typename Func,
+                std::enable_if_t<std::is_invocable_v<Func>, int> = 0>
+        Pipeline &operator<<(Func &&func) {
+            _stages.emplace([f = std::forward<Func>(func)](Dispatcher &) { f(); });
+            if (_stages.size() >= max_stages_in_queue) { run(); }
+            return *this;
+        }
+
+        template<typename Func,
+                std::enable_if_t<std::is_invocable_v<Func, Pipeline &>, int> = 0>
+        Pipeline &operator<<(Func &&func) {
+            func(*this);
+            return *this;
+        }
+
+        void run() {
+            _device->launch([this](Dispatcher &dispatch) {
+                while (!_stages.empty()) {
+                    dispatch.exec(_stages.front());
+                    _stages.pop();
+                }
+            });
+        }
+    };
+
+}
--- Luminous/src/render/samplers/independent.cpp
+++ Luminous/src/render/samplers/independent.cpp
@@ -2,7 +2,7 @@
 // Created by Zero on 2020/10/17.
 //
 
-#include <render/plugin.h>
+#include <render/include/sampler.h>
 #include <compute/dsl.h>
 
 namespace luminous::render {
@@ -10,7 +10,7 @@ namespace luminous::render {
     using namespace compute;
     using namespace compute::dsl;
 
-    class IndependentSampler : public Plugin {
+    class IndependentSampler : public Sampler {
 
     };
 }
