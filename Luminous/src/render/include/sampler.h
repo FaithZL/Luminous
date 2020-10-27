6e7086c50215afceb1eeb14d66165abbc32c9980
Zero <249002031@qq.com>
2020-10-27 19:41:23 +0800

    add independent sampler

--- Luminous/src/render/include/sampler.h
+++ Luminous/src/render/include/sampler.h
@@ -6,26 +6,32 @@
 
 #include <render/plugin.h>
 #include <compute/dsl.h>
+#include <core/header.h>
+#include "compute/pipeline.h"
 
 namespace luminous::render {
     using luminous::compute::Device;
     using luminous::compute::dsl::Expr;
+    using luminous::compute::Pipeline;
 
     class Sampler : public Plugin {
     protected:
         uint _spp;
         uint _cur_frame_idx;
 
+    private:
+        virtual void _prepare_for_next_frame(Pipeline &pipeline) = 0;
+        virtual void _reset(Pipeline &pipeline, uint2 resolution) = 0;
     public:
         Sampler(Device * device, const nloJson &param)
         : Plugin(device, param),
-        _spp(param.value("spp", 8)) {
+        _spp(param.value("spp", 128)) {
 
         }
 
-        [[nodiscard]] uint spp() const noexcept {
-            return _spp;
-        }
+        [[nodiscard]] uint spp() const noexcept { return _spp; }
+
+        [[nodiscard]] uint current_frame_index() const noexcept { return _cur_frame_idx; }
 
         [[nodiscard]] virtual Expr<float> generate_1d_sample(Expr<uint> pixel_index) = 0;
 
@@ -35,8 +41,25 @@ namespace luminous::render {
 
         [[nodiscard]] virtual Expr<float4> generate_4d_sample(Expr<uint> pixel_index) = 0;
 
-        [[nodiscard]] auto reset(uint2 resolution) {
+        [[nodiscard]] auto prepare_for_next_frame() {
+            return [this](Pipeline &pipeline) {
+                pipeline << [this] {
+                    _cur_frame_idx++;
+                    LUMINOUS_WARNING_IF_NOT(_cur_frame_idx <= _spp,
+                                            "Current frame index ",
+                                            _cur_frame_idx,
+                                            " exceeds samples per pixel: ",
+                                            _spp);
+                };
+                _prepare_for_next_frame(pipeline);
+            };
+        }
 
+        [[nodiscard]] auto reset(uint2 resolution) {
+            return [this, resolution](Pipeline &pipeline) {
+                pipeline << [this] { _cur_frame_idx = 0u; };
+                _reset(pipeline, resolution);
+            };
         }
 
     };
--- Luminous/src/render/samplers/independent.cpp
+++ Luminous/src/render/samplers/independent.cpp
@@ -4,13 +4,98 @@
 
 #include <render/include/sampler.h>
 #include <compute/dsl.h>
+#
 
-namespace luminous::render {
+namespace luminous::render::sampler {
 
     using namespace compute;
     using namespace compute::dsl;
 
     class IndependentSampler : public Sampler {
 
+    private:
+        BufferView<uint> _state_buffer;
+        uint _num_dims{};
+
+    private:
+        [[nodiscard]] static Expr<float> _rnd(Expr<uint> prev) noexcept {
+            constexpr auto lcg_a = 1664525u;
+            constexpr auto lcg_c = 1013904223u;
+            prev = (lcg_a * prev + lcg_c);
+            return cast<float>(prev & 0x00ffffffu) * (1.0f / static_cast<float>(0x01000000u));
+        };
+
+        void _prepare_for_next_frame(Pipeline &pipeline) override {
+            /* Nothing to do */
+        }
+
+        void _reset(Pipeline &pipeline, uint2 resolution) override {
+
+            constexpr auto threadgroup_size = make_uint2(16u, 16u);
+            auto pixel_count = resolution.x * resolution.y;
+
+            if (_state_buffer.size() < pixel_count) {
+                _state_buffer = device()->allocate_buffer<uint>(pixel_count);
+            }
+
+            pipeline << device()->compile_kernel("independent_sampler_reset", [&] {
+                auto tea = [](Expr<uint2> val_in) -> Expr<uint> {
+                    Var val = val_in;
+                    Var v0 = val.x;
+                    Var v1 = val.y;
+                    Var s0 = 0u;
+                    for (auto n = 0u; n < 5u; n++) {
+                        s0 += 0x9e3779b9u;
+                        v0 += ((v1 << 4u) + 0xa341316cu) ^ (v1 + s0) ^ ((v1 >> 5u) + 0xc8013ea4u);
+                        v1 += ((v0 << 4u) + 0xad90777du) ^ (v0 + s0) ^ ((v0 >> 5u) + 0x7e95761eu);
+                    }
+                    return v0;
+                };
+                auto txy = thread_xy();
+                If (all(resolution % threadgroup_size == make_uint2(0u)) || all(txy < resolution)) {
+                    auto index = txy.y * resolution.x + txy.x;
+                    _state_buffer[index] = tea(txy);
+                };
+            }).parallelize(resolution, threadgroup_size);
+        }
+    public:
+        IndependentSampler(Device *d, const nloJson &params)
+                : Sampler{d, params} {}
+
+        Expr<float> generate_1d_sample(Expr<uint> pixel_index) override {
+            Var seed = _state_buffer[pixel_index];
+            Var x = _rnd(seed);
+            _state_buffer[pixel_index] = seed;
+            return x;
+        }
+
+        Expr<float2> generate_2d_sample(Expr<uint> pixel_index) override {
+            Var seed = _state_buffer[pixel_index];
+            Var x = _rnd(seed);
+            Var y = _rnd(seed);
+            _state_buffer[pixel_index] = seed;
+            return make_float2(x, y);
+        }
+
+        Expr<float3> generate_3d_sample(Expr<uint> pixel_index) override {
+            Var seed = _state_buffer[pixel_index];
+            Var x = _rnd(seed);
+            Var y = _rnd(seed);
+            Var z = _rnd(seed);
+            _state_buffer[pixel_index] = seed;
+            return make_float3(x, y, z);
+        }
+
+        Expr<float4> generate_4d_sample(Expr<uint> pixel_index) override {
+            Var seed = _state_buffer[pixel_index];
+            Var x = _rnd(seed);
+            Var y = _rnd(seed);
+            Var z = _rnd(seed);
+            Var w = _rnd(seed);
+            _state_buffer[pixel_index] = seed;
+            return make_float4(x, y, z, w);
+        }
     };
 }
+
+LUMINOUS_EXPORT_PLUGIN_CREATOR(luminous::render::sampler::IndependentSampler)
