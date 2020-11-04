909d2b0cfd89c65d41557054a2ac073e4e6fac89
Zero <249002031@qq.com>
2020-11-05 00:11:47 +0800

    Create rasterizer.h

---
+++ Luminous/src/render/include/rasterizer.h
@@ -0,0 +1,17 @@
+//
+// Created by Zero on 2020/11/5.
+//
+
+#pragma once
+
+#include <render/plugin.h>
+#include <compute/dsl.h>
+
+namespace luminous::render {
+    using luminous::compute::Device;
+    using luminous::compute::dsl::Expr;
+
+    class Rasterizer : public Plugin {
+        
+    };
+}
