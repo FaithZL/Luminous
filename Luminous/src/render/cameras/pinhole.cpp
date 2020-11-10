6a5896f1ddd411e55c39801b834eaedb45891d35
Zero <249002031@qq.com>
2020-11-10 22:50:16 +0800

    添加camera

--- Luminous/src/render/cameras/pinhole.cpp
+++ Luminous/src/render/cameras/pinhole.cpp
@@ -2,3 +2,25 @@
 // Created by Zero on 2020/10/19.
 //
 
+#include <compute/dsl_syntax.h>
+#include <render/include/camera.h>
+
+namespace luminous::render::camera {
+    using namespace compute;
+    using namespace compute::dsl;
+    class PinholeCamera : public Camera {
+
+    private:
+        float3 _position;
+        float3 _front;
+        float3 _up;
+        float3 _left;
+        float2 _sensor_size;
+        float _near_plane;
+
+    public:
+
+
+    };
+
+}
--- Luminous/src/render/include/camera.h
+++ Luminous/src/render/include/camera.h
@@ -6,13 +6,43 @@
 
 #include <render/plugin.h>
 #include <compute/dsl.h>
+#include <compute/pipeline.h>
 
 namespace luminous::render {
-    using luminous::compute::Device;
-    using luminous::compute::dsl::Expr;
+    using compute::Ray;
+
+    struct RaySample {
+        Ray ray;
+        float3 throughput;
+    };
+}
+
+LUMINOUS_STRUCT(luminous::render::RaySample, ray, throughput);
+
+namespace luminous::render {
+    using compute::Pipeline;
+    using compute::BufferView;
+    using compute::TextureView;
+    using compute::dsl::Expr;
+    using compute::dsl::Var;
 
     class Camera : public Plugin {
+    private:
+        std::shared_ptr<Film> _film;
+        std::shared_ptr<Filter> _filter;
+        std::shared_ptr<Transform> _transform;
 
+        BufferView<float2> _pixel_position_buffer;
+        BufferView<Ray> _camera_ray_buffer;
+        BufferView<float3> _throughput_buffer;
+        BufferView<float> _pixel_weight_buffer;
+
+        float4x4 _o2w;
+    public:
+        Camera(Device *d, const ParamSet &params)
+                : Plugin{d, params} {
+
+        }
     };
 }
 
