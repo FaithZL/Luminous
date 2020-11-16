2f5aa9bc4ff9fde62c7128aec20ce1d74f2411db
Zero <249002031@qq.com>
2020-11-16 20:40:04 +0800

    implement filter

--- Luminous/src/render/filters/box.cpp
+++ Luminous/src/render/filters/box.cpp
@@ -4,6 +4,16 @@
 
 #include "render/include/filter.h"
 
-namespace luminous::render {
+namespace luminous::render::filter {
+    class BoxFilter : public SeparableFilter {
+    private:
+        [[nodiscard]] float _weight_1d(float offset) const noexcept override {
+            return 1.0f;
+        }
 
+    public:
+        BoxFilter(Device *device, const ParamSet &params) : SeparableFilter{device, params} {}
+    };
 }
+
+LUMINOUS_EXPORT_PLUGIN_CREATOR(luminous::render::filter::BoxFilter);
---
+++ Luminous/src/render/filters/triangle.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Zero on 2020/11/16.
+//
+
+#include "render/include/filter.h"
+
+namespace luminous::render::filter {
+    class TriangleFilter : public SeparableFilter {
+    private:
+        [[nodiscard]] float _weight_1d(float offset) const noexcept override {
+            return std::max(0.0f, radius() - std::abs(offset));
+        }
+
+    public:
+        TriangleFilter(Device *device, const ParamSet &params) :
+        SeparableFilter{device, params} {
+
+        }
+
+    };
+}
